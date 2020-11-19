ea71edfe2a3e0d0c5d0c8a84a9f8c3b717c7fb32
dszarkiel <d.szarkiel@gmail.com>
2020-11-19 15:17:00 -0600

    commit tree c9407366c7d75b22628e8b9ecb8b49cff91d803a

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,11 +6,21 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    if !params[:owner].keys.include?("pet_ids")
+    params[:owner]["pet_ids"] = []
+    end
+
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
